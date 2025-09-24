62e7c9b963de1e36c0611f6e861ff10333795e0f
nongduchuy <nongduchuy203@gmail.com>
2025-09-24 14:22:01 +0700

    nhom-3

---
+++ b4.java
@@ -0,0 +1,46 @@
+
+package btt1;
+
+import java.util.Scanner;
+
+public class b4 {
+    public static void main(String[]args){
+       // Nhân viên 1
+        String hoTen1 = "Nguyen Thi A";
+        String ngaySinh1 = "21/01/2006";
+        String diaChi1 = "Hoang Mai";
+        String gioiTinh1 = "Nam";
+        String maNgach1 = "71112";
+        int bacLuong1 = 3;
+        float heSoLuong1 = 3.33f;
+
+        // Nhân viên 2
+        String hoTen2 = "Nguyen Thi B";
+        String ngaySinh2 = "21/01/2006";
+        String diaChi2 = "Thanh Xuan";
+        String gioiTinh2 = "Nam";
+        String maNgach2 = "71113";
+        int bacLuong2 = 3;
+        float heSoLuong2 = 3.67f;
+
+        // In ra kiểm tra
+        System.out.println("== Thông tin nhân viên 1 ==");
+        System.out.println("Họ tên: " + hoTen1);
+        System.out.println("Ngày sinh: " + ngaySinh1);
+        System.out.println("Địa chỉ: " + diaChi1);
+        System.out.println("Giới tính: " + gioiTinh1);
+        System.out.println("Mã ngạch: " + maNgach1);
+        System.out.println("Bậc lương: " + bacLuong1);
+        System.out.println("Hệ số lương: " + heSoLuong1);
+
+        System.out.println("\n== Thông tin nhân viên 2 ==");
+        System.out.println("Họ tên: " + hoTen2);
+        System.out.println("Ngày sinh: " + ngaySinh2);
+        System.out.println("Địa chỉ: " + diaChi2);
+        System.out.println("Giới tính: " + gioiTinh2);
+        System.out.println("Mã ngạch: " + maNgach2);
+        System.out.println("Bậc lương: " + bacLuong2);
+        System.out.println("Hệ số lương: " + heSoLuong2);
+    }
+    }
+
